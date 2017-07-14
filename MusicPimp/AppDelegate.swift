e33572ab31073d96e83ca5c7e8f09472c769c981
Michael Skogberg <michael.skogberg@futurice.com>
2017-07-15 00:23:05 +0300

    Remove storyboard

--- MusicPimp/AppDelegate.swift
+++ MusicPimp/AppDelegate.swift
@@ -24,6 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var downloadCompletionHandlers: [String: () -> Void] = [:]
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.rootViewController = PimpTabBarController()
         
