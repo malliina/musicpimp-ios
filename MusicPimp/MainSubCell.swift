e9bef88f36e6f2b4c92d0373492fbb945273daa1
Michael Skogberg <michael.skogberg@futurice.com>
2017-09-20 19:23:11 +0300

    Split cell

---
+++ MusicPimp/MainSubCell.swift
@@ -0,0 +1,32 @@
+//
+//  MainSubCell.swift
+//  MusicPimp
+//
+//  Created by Michael Skogberg on 16/09/2017.
+//  Copyright © 2017 Skogberg Labs. All rights reserved.
+//
+
+import Foundation
+
+class MainSubCell: SnapCell {
+    let main = PimpLabel.create()
+    let sub = PimpLabel.create()
+    
+    override func configureView() {
+        installTrackAccessoryView()
+        contentView.addSubview(main)
+        main.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.equalTo(contentView.snp.leadingMargin)
+            make.trailing.equalTo(contentView.snp.trailingMargin)
+        }
+        
+        contentView.addSubview(sub)
+        sub.snp.makeConstraints { make in
+            make.top.equalTo(main.snp.bottom)
+            make.leading.equalTo(contentView.snp.leadingMargin)
+            make.trailing.equalTo(contentView.snp.trailingMargin)
+            make.bottom.equalToSuperview().inset(8)
+        }
+    }
+}
--- MusicPimp/SnapMainSubCell.swift
+++ MusicPimp/SnapMainSubCell.swift
@@ -8,30 +8,7 @@
 
 import Foundation
 
-class SnapMainSubCell: SnapCell {
-    let main = UILabel()
-    let sub = UILabel()
-    
-    override func configureView() {
-        installTrackAccessoryView()
-        main.textColor = PimpColors.titles
-        sub.textColor = PimpColors.titles
-        contentView.addSubview(main)
-        main.snp.makeConstraints { make in
-            make.top.equalToSuperview()
-            make.leading.equalTo(contentView.snp.leadingMargin)
-            make.trailing.equalTo(contentView.snp.trailingMargin)
-        }
-        
-        contentView.addSubview(sub)
-        sub.snp.makeConstraints { make in
-            make.top.equalTo(main.snp.bottom)
-            make.leading.equalTo(contentView.snp.leadingMargin)
-            make.trailing.equalTo(contentView.snp.trailingMargin)
-            make.bottom.equalToSuperview().inset(8)
-        }
-    }
-    
+class SnapMainSubCell: MainSubCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         super.removeAccessoryMargin()
