991eb2eb9c5200b43f262e1287ec2b72bc594090
Michael Skogberg <michael.skogberg@futurice.com>
2019-03-11 20:44:55 +0200

    Try to fix bridging for appcenter

--- MusicPimp-Bridging-Header.h
+++ MusicPimp-Bridging-Header.h
@@ -6,14 +6,14 @@
 //  Copyright (c) 2015 Skogberg Labs. All rights reserved.
 //
 
-#ifndef MusicPimp_SocketRocket_Bridging_Header_h
-#define MusicPimp_SocketRocket_Bridging_Header_h
+//#ifndef MusicPimp_SocketRocket_Bridging_Header_h
+//#define MusicPimp_SocketRocket_Bridging_Header_h
 
-#import "SocketRocket/SRWebSocket.h"
+//#import "SocketRocket/SRWebSocket.h"
 #import "NSString+FontAwesome.h"
 #import "UIFont+FontAwesome.h"
 #import "UIImage+FontAwesome.h"
 #import "MusicPimp/ColorScheme/DBImageColorPicker.h"
 #import "MusicPimp/ColorScheme/UIImage+Color.h"
 
-#endif
+//#endif
